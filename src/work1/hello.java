1d64de78da176c88d978b17b89f695fbafc6728e
nuoztu <nuoztu@gmail.com>
2021-05-31 12:44:30 -0500

    Added more work

--- src/work1/hello.java
+++ src/work1/hello.java
@@ -3,5 +3,7 @@ package work1;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Work1 started");
+        System.out.println("Created git repository");
+        System.out.println("");
     }
 }
